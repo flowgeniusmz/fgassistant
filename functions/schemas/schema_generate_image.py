cda0faa45cf73f4e1f8d2d52fabfb8daa77186b8
flowgeniusmz <128870971+flowgeniusmz@users.noreply.github.com>
2023-12-09 00:57:39 -0600

    Update schema_generate_image.py

--- functions/schemas/schema_generate_image.py
+++ functions/schemas/schema_generate_image.py
@@ -1,23 +1,20 @@
-schema_generate_image = {
-    "type": "function",
-    "function": {
-        "name": "generate_image",
-        "description": "Generates an image using Dall-e 3 based on a provided text prompt.",
-        "parameters": {
-            "type": "object",
-            "properties": {
-                "prompt": {
-                    "type": "string",
-                    "description": "The text prompt based on which the image is to be generated."
-                },
-                "size": {
-                    "type": "string",
-                    "enum": ["c", "f"],
-                    "description": "The size of the generated image. Allowed values are 'c' and 'f'."
-                }
+{
+    "name": "generate_image",
+    "description": "Generates an image using Dall-e 3 based on a provided text prompt.",
+    "parameters": {
+        "type": "object",
+        "properties": {
+            "prompt": {
+                "type": "string",
+                "description": "The text prompt based on which the image is to be generated."
             },
-            "required": ["prompt"]
-        }
+            "size": {
+                "type": "string",
+                "enum": ["c", "f"],
+                "description": "The size of the generated image. Allowed values are 'c' and 'f'."
+            }
+        },
+        "required": ["prompt"]
     }
 }
 
