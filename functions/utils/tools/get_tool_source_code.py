6101cfbec5093f13e0058dcca1cfb1f871e6da1e
flowgeniusmz <128870971+flowgeniusmz@users.noreply.github.com>
2023-12-09 02:03:08 -0600

    Update get_tool_source_code.py

--- functions/utils/tools/get_tool_source_code.py
+++ functions/utils/tools/get_tool_source_code.py
@@ -1,5 +1,5 @@
 import inspect
 
 def get_tool_source_code(varFunction):
-  source_code = inspect.sourcecode(varFunction)
-  return source_code
+    source_code = inspect.getsource(varFunction)
+    return source_code
