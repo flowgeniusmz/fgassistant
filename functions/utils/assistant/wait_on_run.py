cdff355d540979176a168ed0b3ca141246eb0039
flowgeniusmz <128870971+flowgeniusmz@users.noreply.github.com>
2023-12-08 22:49:36 -0600

    Create wait_on_run.py

---
+++ functions/utils/assistant/wait_on_run.py
@@ -0,0 +1,13 @@
+import time
+from openai import OpenAI
+
+client = OpenAI(api_key = st.secrets.openai.api_key)
+
+def wait_on_run(run, thread):
+    while run.status == "queued" or run.status == "in_progress":
+        run = client.beta.threads.runs.retrieve(
+            thread_id=thread.id,
+            run_id=run.id,
+        )
+        time.sleep(0.5)
+    return run
